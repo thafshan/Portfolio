97e800ff8ed950f1b04b56e899698bab676cd0ea
thafshan <afshankhanyt0004@gmail.com>
2025-12-07 16:38:21 +0530

    pic removed

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,11 +6,11 @@ const Home = () => {
   return (
     <div id='home'>
       <section className={styles.homeSection}>
-        <div className={styles.homeLeft}>
+        <div className={styles.homeContent}>
           <h1 className={styles.homeHeading}>
-            <span>Hello, it's Me</span>
+            <span className={styles.w}>Hello, it's Me</span>
             <span className={styles.name}>Afshan Thayab Khan</span>
-            <span>And I'm a</span>
+            <span className={styles.w}>And I'm a</span>
             <span className={styles.role}>Frontend Developer</span>
           </h1>
           <ul className={styles.socialList}>
@@ -30,9 +30,6 @@ const Home = () => {
               </a>
             </li>
           </ul>
-        </div>
-        <div className={styles.homeRight}>
-          <img className={styles.homeImg} src="/images/PFP.png" alt="photo" />
         </div>
       </section>
     </div>
